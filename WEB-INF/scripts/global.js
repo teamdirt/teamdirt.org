618bc8ec87d73a2fe00dbe7fae6435d2c45d9066
Gregory Gutshall <gregory.gutshall@gmail.com>
2017-04-03 16:47:04 -0700

    Added Hero Banner Control and insertions.

---
+++ WEB-INF/scripts/global.js
@@ -0,0 +1,43 @@
+// Define the Application
+var tdApp = angular.module('tdApp', []);
+
+function HeroBanner() {
+    var slideIndex = 0;
+    
+    this.images = [
+        {name:"X", src:"/imgs/heroBanner/bareTrail1.jpg"},
+        {name:"X", src:"/imgs/heroBanner/building_1.jpg"},
+        {name:"X", src:"/imgs/heroBanner/eqp_2.jpg"},
+        {name:"X", src:"/imgs/heroBanner/events_1.jpg"}
+    ];
+    
+    this.getCurrentImg = function () {
+        return this.images[slideIndex].src;
+    }
+    
+    this.shift = function(n) {
+        if (n + slideIndex > this.images.length - 1) {
+            slideIndex = 0;
+        } else if (n + slideIndex < 0) {
+            slideIndex = this.images.length - 1;       
+        } else {
+            slideIndex += n;
+        }
+    }
+
+    this.set = function(n) {
+        slideIndex = n;
+    }
+    
+    this.getColorCode = function(n) {
+        if (n == slideIndex) {
+            return "w3-white";
+        }
+        return "";
+    }
+      
+}
+
+
+// Add the Hero Banner Controller
+tdApp.controller('HeroBanner', HeroBanner);
