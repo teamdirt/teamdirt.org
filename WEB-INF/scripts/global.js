74e654001ff8f9628176e96d6df75d04cef2176c
microDude <gregory.gutshall@gmail.com>
2017-04-03 22:51:49 -0700

    added more static content

--- WEB-INF/scripts/global.js
+++ WEB-INF/scripts/global.js
@@ -5,7 +5,7 @@ function HeroBanner() {
     var slideIndex = 0;
     
     this.images = [
-        {name:"X", src:"/imgs/heroBanner/bareTrail1.jpg"},
+        {name:"X", src:"/imgs/placeholder.png"},
         {name:"X", src:"/imgs/heroBanner/building_1.jpg"},
         {name:"X", src:"/imgs/heroBanner/eqp_2.jpg"},
         {name:"X", src:"/imgs/heroBanner/events_1.jpg"}
