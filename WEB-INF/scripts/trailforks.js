244472f160529310fe8aef715d240016e8f5db83
Gregory Gutshall <gregory.gutshall@gmail.com>
2017-04-04 17:12:31 -0700

    added first pass at the trails pages

---
+++ WEB-INF/scripts/trailforks.js
@@ -0,0 +1,4 @@
+var script = document.createElement("script"); 
+script.setAttribute("src", "https://es.pinkbike.org/ttl-86400/sprt/j/trailforks/widget.js"); 
+document.getElementsByTagName("head")[0].appendChild(script); 
+var widgetCheck = false;
