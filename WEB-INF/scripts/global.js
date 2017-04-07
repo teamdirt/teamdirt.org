a489e53e81c1a9c8f8fa3e9f3464bc89de956132
Gregory Gutshall <gregory.gutshall@gmail.com>
2017-04-07 16:48:08 -0700

    header imgs

--- WEB-INF/scripts/global.js
+++ WEB-INF/scripts/global.js
@@ -4,17 +4,28 @@ var tdApp = angular.module('tdApp', []);
 function HeroBanner() {
     var slideIndex = 0;
     
+	// We want the Hero to be 1600x!
+	//convert -resize 1600x bareTrail2.jpg bareTrail2.jpg
     this.images = [
-        {name:"X", src:"/imgs/placeholder.png"},
-        {name:"X", src:"/imgs/heroBanner/building_1.jpg"},
-        {name:"X", src:"/imgs/heroBanner/eqp_2.JPG"},
-        {name:"X", src:"/imgs/heroBanner/events_1.jpg"}
+		{name:"Shuttle Days", src:"/imgs/raw_imgs/equipment/eqp_2.JPG"},
+        {name:"A Cool Img", src:"/imgs/raw_imgs/bare_trail/bareTrail1.jpg"},
+		{name:"A Cool Img Too", src:"/imgs/raw_imgs/bare_trail/bareTrail2.jpg"},
+        {name:"Building", src:"/imgs/raw_imgs/building/building_1.jpg"},
+        {name:"An Event", src:"/imgs/raw_imgs/events/events_1.jpg"}
     ];
     
     this.getCurrentImg = function () {
         return this.images[slideIndex].src;
     }
-    
+	
+	this.getCurrentImgName = function () {
+		return this.images[slideIndex].name;
+	}
+	
+	this.getBackgroundImg = function () {
+		return {'background-image':'url(' + this.getCurrentImg() +')' };
+	}
+	
     this.shift = function(n) {
         if (n + slideIndex > this.images.length - 1) {
             slideIndex = 0;
