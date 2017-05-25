5feb0008d22bfc7a9b65332c4f024493707cca06
Gregory Gutshall <gregory.gutshall@gmail.com>
2017-05-25 09:37:53 -0700

    Changed to PayPal donation link

--- WEB-INF/scripts/mainpage.js
+++ WEB-INF/scripts/mainpage.js
@@ -1,8 +1,3 @@
-var script = document.createElement("script"); 
-script.setAttribute("src", "https://es.pinkbike.org/ttl-86400/sprt/j/trailforks/widget.js"); 
-document.getElementsByTagName("head")[0].appendChild(script); 
-var widgetCheck = false;
-
 function HeroBanner() {
     var slideIndex = 0;
     
